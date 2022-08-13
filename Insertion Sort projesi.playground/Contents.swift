78a8f2f13a26dde7ffb49e70a5bd1e4da60b6ae5
hakaneerdogmus <hakan.eerdogmus@gmail.com>
2022-08-13 18:00:35 +0300

    www.patika.dev Insertion Sort Projesi
    www.patika.dev

---
+++ Insertion Sort projesi.playground/Contents.swift
@@ -0,0 +1,28 @@
+1. Soru
+
+            [22,27,16,2,18,6]
+
+    [2,27,16,22,18,6] [2,6,16,22,18,27] [2,6,16,18,22,27]
+
+2. Soru
+
+    O(n^2) olarak belirlenir.
+
+3. Soru
+
+    Average case: Aradığımız sayının ortada olması = 18
+    Worst case: Aradığımız sayının sonda olması = 27
+    Best case: Aradığımız sayının dizinin en başında olması = 2
+
+4. Soru
+
+    Average kapsamına girer çünkü sayımız ortadadır.
+            [2,6,16,18,22,27]
+
+
+[7,3,5,8,2,9,4,15,6] insertion sort’a göre ilk 4 adım:
+
+[2,3,5,8,7,9,4,15,6]
+[2,3,4,8,7,9,5,15,6]
+[2,3,4,5,7,9,8,15,6]
+[2,3,4,5,6,9,8,15,7]
